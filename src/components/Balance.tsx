14071cba5e039a02c0925012490bc6059e9d07a0
rahul <rahul.anagani2006@gmail.com>
2025-07-06 00:21:49 +0530

    api

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { PieChart,ResponsiveContainer,Pie,Tooltip,Legend,Cell } from "recharts"
 export default function Balance(){
     const d={ name: 'JUL', food: 1000, medical: 300, groceries: 0, transport: 200, rent: 0, other: 1000 }
     const data=Object.entries(d).filter(([key])=>(key!=="name" && key!=="month")).map(([key,value])=>({name:key,value:value}));
-    const budget=2500;
-        const spent=Object.entries(d).filter(([key])=>(key!=="name" && key!=="month")).reduce((total:number,[key,value])=>total+parseInt(value as string),0);
+    const [budget,setBudget]=useState<0>(0);
+    const date=new Date;
+    useEffect(()=>{
+        const fetchBdgt=async()=>{
+            const resp=await fetch(`/api/budget?year=${date.getFullYear()}&month=${date.getMonth()+1}`);
+            const data=await resp.json();
+            console.log(data)
+            setBudget(data.budget[0].budget)
+        }
+        fetchBdgt()
+    },[])
+
+    const spent=Object.entries(d).filter(([key])=>(key!=="name" && key!=="month")).reduce((total:number,[key,value])=>total+parseInt(value as string),0);
     const COLORS = [
     "#ea580c",
     "#2563eb",
@@ -17,7 +29,7 @@ export default function Balance(){
                                     <div className="w-[60%] h-full flex items-start px-10 justify-center flex-col">
                                         <h1 className="font-bold text-white text-3xl roboto">Available</h1>
                                         <h1 className="text-white self-center font-light roboto">This month</h1>
-                                        <h1 className="font-bold text-white self-end text-3xl roboto">${budget}</h1>
+                                        <h1 className="font-bold text-white self-end text-3xl roboto">${budget.toPrecision(4)}</h1>
                                     </div>
                                     <div className="w-[40%] flex justify-center items-start p-5">
                                         <button className="roboto bg-white/20 p-2 rounded-2xl hover:bg-white/10 active:bg-white/10 cursor-pointer text-gray-200 font-medium ">
--- src/components/Monthly.tsx
+++ src/components/Monthly.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { ResponsiveContainer,BarChart,Bar,CartesianGrid,XAxis,YAxis,Tooltip,Legend } from "recharts"
 type spendingData={
      name: string, food: number, medical: number, groceries: number, transport: number, rent: number, other: number ,month:number
@@ -5,14 +6,24 @@ type spendingData={
 type props={
     spendingData:spendingData
 }
-export default function Monthly({spendingData}:props){
+export default function Monthly(){
+    const [spendingData,setSpendingData]=useState<spendingData[]>([]);
+    const date=new Date;
+    useEffect(()=>{
+        const fetchTransactions=async()=>{
+            const resp=await fetch(`/api/getTransactions?year=${date.getFullYear()}`,{method:"GET"});
+            const data=await resp.json();
+            setSpendingData(data.spendingData)
+        }
+        fetchTransactions();
+    },[])
     return <div className="h-[50dvh] sm:h-[50dvh] lg:h-[50%] md:h-[30%] w-full rounded-2xl bg-emerald-800">
                                     <ResponsiveContainer>
                                         <BarChart
                                         width={"100%"}
                                         height={"100%"}
                                         className=""
-                                        data={spendingData}
+                                        data={spendingData.slice(0,6)}
                                         margin={{
                                             top: 20,
                                             right: 30,
@@ -26,9 +37,9 @@ export default function Monthly({spendingData}:props){
                                             <Legend />
                                                 <Bar dataKey="food" stackId="a" radius={[0, 0, 0, 0]} fill="#f9c74f" /> 
                                                 <Bar dataKey="medical" stackId="a" radius={[0, 0, 0, 0]} fill="#f9844a" />   
-                                                <Bar dataKey="groceries" stackId="a" radius={[0, 0, 0, 0]} fill="#90be6d" />   
+                                                <Bar  dataKey="groceries" stackId="a" radius={[0, 0, 0, 0]} fill="#90be6d" />   
                                                 <Bar dataKey="transport" stackId="a" radius={[10, 10, 0, 0]} fill="#577590" />
-    
+
                                         </BarChart>
                                     </ResponsiveContainer>
                                 </div>
